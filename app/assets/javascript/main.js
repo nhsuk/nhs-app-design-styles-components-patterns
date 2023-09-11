cf720251a1998ae1e0ffea3d6337d30af77bf661
Dave Hunter <thebeakking@gmail.com>
2023-09-11 19:24:52 +0100

    add js for dynamic page title greeting

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,21 @@
 // ES6 or Vanilla JavaScript
+
+// Greeting message
+
+var thehours = new Date().getHours();
+  var themessage;
+  var morning = ('Good morning');
+  var afternoon = ('Good afternoon');
+  var evening = ('Good evening');
+
+  if (thehours >= 0 && thehours < 12) {
+    themessage = morning; 
+
+  } else if (thehours >= 12 && thehours < 17) {
+    themessage = afternoon;
+
+  } else if (thehours >= 17 && thehours < 24) {
+    themessage = evening;
+  }
+
+  $('.greeting').append(themessage);

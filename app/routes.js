fab39b7d046dedf96fc23008e0e46f6ae6d33825
Dave Hunter <thebeakking@gmail.com>
2023-09-18 11:03:49 +0100

    add routing logic for versioning

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,25 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+// Adding "query" to every page. There could be a better implementaion using middleware. 
+// in nunjkucks you can call {{query[key]}}
+
+router.get('*', function(req, res) {
+  let path = req.params[0]
+  let pathEnd = path.slice(-1)
+  let newPath = path.substring(1)
+  if (path == "/") {
+    // Top home page
+    path = "index"
+  } else if (pathEnd == "/") {
+    // Check if this is the route folder, if so, then render the index page.
+    path = newPath + "index"
+  } else {
+    path = newPath
+  }
+  res.render(path, {
+    "query": req.query,
+  });
+})
+
 module.exports = router;

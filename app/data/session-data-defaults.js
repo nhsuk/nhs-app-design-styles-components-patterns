74adbea0e851c8c159753f25a05d0b7935b04e17
Dave Hunter <thebeakking@gmail.com>
2023-09-20 10:48:28 +0100

    add account and settings pages for p5 and p9

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -5,13 +5,17 @@ module.exports = {
   // Defaults page view to native
   "web": "no", 
 
-  // Set name and age for linked profile
-  "linkedProfileName": "Lyra Hunter",
+  // Set linked profile details
+  "linkedProfileFirstName": "Lyra",
+  "linkedProfileLastName": "Hunter",
   "linkedProfileAge": "5",
 
-  // Set name and age for user
+  // Set user details
   "userFirstName": "Hamish",
   "userLastName": "Hunter",
-  "nhsNumber": "123 456 7890"
+  "nhsNumber": "123 456 7890",
+
+  // Set version of the app
+  "version": "2.36.6 (2.36.0)",
 
 }

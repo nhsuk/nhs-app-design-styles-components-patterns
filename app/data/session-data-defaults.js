177c88126953c2dc34b1d822f2388b6d991f8dbb
Dave Hunter <thebeakking@gmail.com>
2023-09-20 07:52:03 +0100

    add default session data for linked profiles

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,6 +1,8 @@
 module.exports = {
 
   // Insert values here
-  "web": "no"
+  "web": "no", // Defaults page view to native
+  "linkedProfileName": "Lyra Hunter", // Sets name for linked profile
+  "linkedProfileAge": "5" // Sets age for linked profile
 
 }

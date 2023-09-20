b4c991a394ed80116574ea351ab9323d5f165a92
Dave Hunter <thebeakking@gmail.com>
2023-09-20 09:13:41 +0100

    add default session data for user name and nhs number

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,8 +1,17 @@
 module.exports = {
 
   // Insert values here
-  "web": "no", // Defaults page view to native
-  "linkedProfileName": "Lyra Hunter", // Sets name for linked profile
-  "linkedProfileAge": "5" // Sets age for linked profile
+
+  // Defaults page view to native
+  "web": "no", 
+
+  // Set name and age for linked profile
+  "linkedProfileName": "Lyra Hunter",
+  "linkedProfileAge": "5",
+
+  // Set name and age for user
+  "userFirstName": "Hamish",
+  "userLastName": "Hunter",
+  "nhsNumber": "123 456 7890"
 
 }

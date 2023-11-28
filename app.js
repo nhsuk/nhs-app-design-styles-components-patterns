a30b1e07f3e96ee6f2904e7cf3a5b8c09f27f8ac
Dave Hunter <thebeakking@gmail.com>
2023-11-28 09:34:14 +0000

    add clear data link

--- app.js
+++ app.js
@@ -207,10 +207,10 @@ if (useDocumentation || onlyDocumentation === 'true') {
 }
 
 // Clear all data in session if you open /examples/passing-data/clear-data
-app.post('/examples/passing-data/clear-data', (req, res) => {
-  req.session.data = {};
-  res.render('examples/passing-data/clear-data-success');
-});
+app.post('/clear-data', function (req, res) {
+  req.session.data = {}
+  res.render('clear-data-success')
+})
 
 // Redirect all POSTs to GETs - this allows users to use POST for autoStoreData
 app.post(/^([^.]+)$/, (req, res) => {

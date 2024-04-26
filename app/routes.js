da85dcb22ccb23be659b8c2af2c89929244a226e
Dave Hunter <thebeakking@gmail.com>
2024-04-26 14:02:54 +0100

    update gp appointment pages

--- app/routes.js
+++ app/routes.js
@@ -26,4 +26,30 @@ router.get('*', function(req, res) {
   });
 })
 
+// GP Appointments - Appointment type
+router.post('/pages/services/gp-appointment-type-answer', function (req, res) {
+
+  var install = req.session.data['gp-appointment-type']
+
+  if (install == "Routine GP"){
+    res.redirect('/pages/services/gp-appointment-select-appointment-v3')
+  } else if (install == "none") {
+    res.redirect('/pages/services/gp-appointment-type-not-available')
+  } else {
+    res.redirect('/pages/services/gp-appointment-invite')
+  }
+})
+
+// GP Appointments - Invited to this appointment?
+router.post('/pages/services/gp-appointment-invite-answer', function (req, res) {
+
+  var install = req.session.data['gp-appointment-invite']
+
+  if (install == "No"){
+    res.redirect('/pages/services/gp-appointment-type-not-available')
+  } else {
+    res.redirect('/pages/services/gp-appointment-select-appointment-v3')
+  }
+})
+
 module.exports = router;

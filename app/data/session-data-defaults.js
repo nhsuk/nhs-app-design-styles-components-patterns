c098e92bdb9a1cc9651e1d47a82fe671783df696
Dave Hunter <thebeakking@gmail.com>
2024-07-10 13:00:13 +0100

    add medicine quantity

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -39,20 +39,26 @@ module.exports = {
   // Medicines
   "medicine1": "Felodipine 2.5micrograms tablets",
   "medicine1description": "10 tablet",
+  "medicine1quantity": "10 tablet",
 
   "medicine2": "Atorvastatin 500mg capsules",
   "medicine2description": "Take ONE capsule THREE times daily for 7 days, 28 capsules",
+  "medicine2quantity": "28 capsules",
 
   "medicine3": "Nuelin SA 175mg tablets (Meda Pharmaceuticals Ltd)",
   "medicine3description": "Two to be taken twice a day 60 tablets",
+  "medicine3quantity": "60 tablets",
 
   "medicine4": "Warfarin 500microgram tablets",
   "medicine4description": "take as instructed by anticoagulant clinic 2 tablets",
+  "medicine4quantity": "2 tablets",
 
-  "medicine5": "Tamsulosin 400microgram modified release capsules",
-  "medicine5description": "1 OD - 56 capsules",
+  "medicine5": "Ramipril 5mg capsules",
+  "medicine5description": "take as instructed 10 tablets",
+  "medicine5quantity": "10 tablets",
 
-  "medicine6": "Ramipril 5mg capsules",
-  "medicine6description": "take as instructed 10 tablets",
+  "medicine6": "Tamsulosin 400microgram modified release capsules",
+  "medicine6description": "1 OD - 56 capsules",
+  "medicine6quantity": "1 OD - 56 capsules",
 
 }
